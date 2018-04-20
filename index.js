4279bb73667f75f93a50f48e3e13736ae06271eb
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-20 23:19:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  const a = document.querySelectorAll('.ranked-list li');
+  for(let i = 0;i<a.length;i++) {
+    a[i].innerHTML = parseInt(a[i].innerHTML)+n;
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div');
+}
