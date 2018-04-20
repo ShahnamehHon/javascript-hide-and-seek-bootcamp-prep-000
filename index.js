c3da03d9c414ebd1e24dc1ca3065eeb8c50c44d0
Ahmadali <ahmadali.azizov@outlook.com>
2018-04-20 23:00:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const a = document.querySelectorAll('.ranked-list li');
   for(let i = 0;i<a.length;i++) {
-    a[i].innerHTML = parseInt(a[i].innerHTML)
+    a[i].innerHTML = parseInt(a[i].innerHTML)+n;
   }
 }
